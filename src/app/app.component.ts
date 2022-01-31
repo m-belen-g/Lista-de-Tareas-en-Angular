e5cc21f5a4e369ce48b42c655a0d822d063c4c17
M. Belen Gomez <maria.belen-gomez@hotmail.com>
2022-01-31 17:51:27 -0300

    Primer commit del proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Lista de tareass';
+
+  tareas: any[] = []
+  crearTarea(tarea: String, $event: any) {
+    $event.preventDefault(); // para prevenir reloading
+    this.tareas.push(
+      {
+        id: Date.now(), // generamos un id en base al momento actua
+        texto: tarea
+      }
+    );
+    console.log(this.tareas);
+  }
+  eliminarTarea(id: number) {
+    console.log(id);
+    this.tareas = 
+      this.tareas.filter(tarea => tarea.id !== id);
+  }
+}
+
+
